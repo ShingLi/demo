8269571477833992a4a008604ac3aab6234553eb
shingli <745573545@qq.com>
2018-09-04 18:07:05 +0800

    first commit

---
+++ src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="header_wrapper">
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'header'
+    }
+</script>
+<style lang="scss" scoped>
+    .header_wrapper{}
+</style>
